6edd17f890900c86d17c2f01f98dbc0ef9b0907a
Elijah Malaby <qwe12345678910@gmail.com>
2018-02-02 08:48:36 -0500

    Possible fix for matrix attribute stride issue

--- core/internals.lisp
+++ core/internals.lisp
@@ -18,9 +18,9 @@
                                                        instance-divisor)
   (let ((type (varjo:type-spec->type array-type)))
     (if (and (varjo:core-typep type) (not (varjo:v-typep type 'v-sampler)))
-        (let ((slot-layout (cepl.types::expand-slot-to-layout
-                            nil type normalized))
-              (stride 0))
+        (let* ((slot-layout (cepl.types::expand-slot-to-layout)
+                            nil type normalized)
+               (stride (reduce #'+ slot-layout :key #'car)))
           (loop :for attr :in slot-layout
              :for i :from 0
              :with offset = 0
